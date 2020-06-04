2e62c1b7047c94fe7c704c807bdc9c8b9c767352
Rupesh Kumar Tiwari <roopkt@gmail.com>
2020-06-04 15:54:30 -0400

    layout flexbox

--- App.js
+++ App.js
@@ -1,9 +1,32 @@
 import React from 'react';
 import { Text, View } from 'react-native';
 
+const restaurants = [
+  { name: 'React Cafe', address: '123 Anywhere St' },
+  { name: 'Fancy Restaurant', address: '799 Main St' },
+  { name: 'Taco Place', address: '550 Maple Rd' },
+  { name: "Tony's Diner", address: '4101 College St' },
+  { name: 'Pasta Central', address: '706 Harper St' },
+  { name: 'Burger Builder', address: '4869 Hamilton Dr' },
+  { name: 'Pizza Express', address: '1049 Bird St' },
+  { name: 'Teriyaki To Go', address: '1885 Tea Berry Lane' },
+  { name: 'Maroon Deli', address: '1082 Stuart St' },
+  { name: 'Prime Bar and Grill', address: '1848 Fairfax Dr' },
+  { name: 'Dumpling House', address: '747 Kelly Dr' },
+  { name: 'Hot Chicken', address: '1816 Olive St' },
+  { name: "Luna's Tap Room", address: '3256 Spirit Dr' },
+  { name: 'Quick Sandwich Shop', address: '2587 Cherry Ridge Dr' },
+  { name: "Bobby's Burgers", address: '4152 Berkley St' },
+  { name: 'Turnpike Diner', address: '4571 Central Ave' },
+  { name: 'Bombay Express', address: '65 Queens Lane' },
+  { name: 'Coffee Central', address: '3228 Oakwood Circle' },
+  { name: "King's Garden", address: '2935 Victoria Ct' },
+  { name: 'Salads and More', address: '2454 Preston St' },
+];
+
 export default function App() {
   return (
-    <View>
+    <View style={{ backgroundColor: 'orange', flex: 1 }}>
       <Text
         style={{
           padding: 40,
@@ -15,11 +38,35 @@ export default function App() {
       >
         Restaurant Review
       </Text>
+      {restaurants.map((place, index) => {
+        return (
+          <View key={place.name} style={{ flexDirection: 'row' }}>
+            <View
+              style={{
+                flex: 1,
+                alignItems: 'center',
+                justifyContent: 'center',
+              }}
+            >
+              <Text>{index + 1}</Text>
+            </View>
 
-      <Text>React Cafe</Text>
-      <Text style={{ color: 'grey' }}>123 Anywhere St</Text>
-      <Text> Fancy Restaurant</Text>
-      <Text style={{ color: 'grey' }}>799 Main St</Text>
+            <View style={{ flexDirection: 'column', flex: 8 }}>
+              <Text>{place.name}</Text>
+              <Text style={{ color: 'grey' }}>{place.address}</Text>
+            </View>
+            <View
+              style={{
+                flex: 1,
+                alignItems: 'center',
+                justifyContent: 'center',
+              }}
+            >
+              <Text>Info</Text>
+            </View>
+          </View>
+        );
+      })}
     </View>
   );
 }
--- README.md
+++ README.md
@@ -4,3 +4,15 @@
 
 - only Text can be nested within the Text component. 
 - Any Text you want toshow should be within Text component only.
+
+
+### Chapter-3 Layout React Native Component With FlexBox
+
+- Create Restaurant constant and render views
+- Info Button right & text and address in middle
+- Default flex direction is column. On the web it is row
+- Use FlexDirection Row for Restaurants
+- Change Name & Address stack as column
+  - Take most of the realstate
+- Name, number, info  they take equal amount. Adjust them 
+- Flex styles are only applied on view not in text. 
