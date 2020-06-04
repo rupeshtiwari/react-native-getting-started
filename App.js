452887a8da19fb0fa14059c0040c1035f2530fca
Rupesh Kumar Tiwari <roopkt@gmail.com>
2020-06-04 15:24:12 -0400

    added new class and style

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text style={styles.welcome}>Restaurant Review App</Text>
     </View>
   );
 }
@@ -15,5 +15,11 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+
+  welcome: {
+    fontSize: 20,
+    textAlign: 'center',
+    margin: 10,
   },
 });
